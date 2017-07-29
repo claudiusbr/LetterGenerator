9d01c361a933763a47dfbadbd7a200c99f0fad4b
claudiusbr <claudiusbr@users.noreply.github.com>
2017-07-29 23:37:49 +0100

    passing test

--- src/test/scala/renderer/IntMedTester.scala
+++ src/test/scala/renderer/IntMedTester.scala
@@ -4,15 +4,23 @@ import scala.swing.MainFrame
 
 import org.scalatest.{FunSpec, GivenWhenThen}
 import org.mockito.Mockito.when
+import org.mockito.Mockito
 import org.scalatest.mockito.MockitoSugar
 
 class IntMedTester extends FunSpec 
   with GivenWhenThen with MockitoSugar{
   
-  //failing test
+  val mockFrame: MainFrame = mock[MainFrame]
+  
+  val im = InteractionMediator(mockFrame)
+
   describe("the runInterface method") {
     it("should run the GUI") {
-      assert(false)
+      When("it is called")
+      im.runInterface()
+      
+      Then("the frame should become visible")
+      Mockito.verify(mockFrame, Mockito.times(1)).visible_=(true)
     }
   }
 
