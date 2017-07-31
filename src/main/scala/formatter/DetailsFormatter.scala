b088b09234a3a1033258446e6da836d13627d6f3
claudiusbr <claudiusbr@users.noreply.github.com>
2017-07-31 17:15:51 +0100

    added formatter class

---
+++ src/main/scala/formatter/DetailsFormatter.scala
@@ -0,0 +1,44 @@
+package formatter
+
+import scala.collection.mutable.{HashMap => mHashMap,ArrayBuffer}
+import scala.collection.immutable.HashMap
+import scala.util.matching.Regex
+
+class DetailsFormatter(input: Input) {
+
+  val details: Map[String,Array[String]] = {
+
+    val it: Iterator[String] = input.getContents().toIterator
+
+    val det: mHashMap[String,ArrayBuffer[String]] = mHashMap()
+
+    val headers: Array[String] = it.next.split(",")
+
+    for (element <- headers) {
+      det(element) = ArrayBuffer[String]()
+    }
+
+    // fill up columns with values from input
+    while (it.hasNext) {
+      val tuple: Array[String] = replCommas(it.next).split(",")
+
+      for (i <- tuple.indices) det(headers(i)) += tuple(i)
+    }
+
+    det.map(e => (e._1, e._2.toArray)).toMap
+  }
+  
+  def replCommas(text: String): String = {
+    // this is needed to avoid wrong splitting of any line
+    val commaInText: Regex = "(.*\".*),(.*\".*)".r
+
+    // this is where the regular expression comes in
+    val formatted = commaInText.replaceAllIn(text, _ => s"$$1;$$2")
+
+    commaInText.findFirstIn(formatted) match {
+      case Some(e) => replCommas(e)
+      case None => formatted
+    }
+  }
+
+}
