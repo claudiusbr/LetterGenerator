548f8c427659e631d5dd7dc0162c155115ef0d69
claudiusbr <claudiusbr@users.noreply.github.com>
2017-08-01 18:07:33 +0100

    dummy implementation of DocxInput

--- src/main/scala/formatter/Input.scala
+++ src/main/scala/formatter/Input.scala
@@ -2,10 +2,21 @@ package formatter
 
 trait Input {
   def getContents(): Iterator[String]
+  def getFile(): Any
 }
 
-class CsvInput(file: String) extends Input {
-  override def getContents(): Iterator[String] = {
-      io.Source.fromFile(file).getLines
-  }
+class CsvInput(fileName: String) extends Input {
+  override def getContents(): Iterator[String] = 
+    io.Source.fromFile(fileName).getLines
+
+  override def getFile(): Any = getContents()
+}
+
+class DocxInput(fileName: String) extends Input {
+  import org.docx4j.openpackaging.packages.{WordprocessingMLPackage => WordPack}
+  
+
+
+  override def getContents(): Iterator[String] = ???
+  override def getFile(): Any = ???
 }
