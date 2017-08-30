fcb490736f7a441497d9fc5464c6d5efc3af9e68
claudiusbr <claudiusbr@users.noreply.github.com>
2017-08-31 00:08:05 +0100

    green test for validateAllPaths

--- src/test/scala/lettergenerator/mediator/ValMedTester.scala
+++ src/test/scala/lettergenerator/mediator/ValMedTester.scala
@@ -18,17 +18,17 @@ class ValMedTester extends FunSpec
   
   val vm = new ValidationMediator(mockGui)
   
-  when(mockPathValidator.validate(
-    Matchers.matches("./valid/path")))
-    .thenReturn(true)
-
-  when(mockPathValidator.validate(
-    not(Matchers.eq("./valid/path"))))
-    .thenReturn(false)
-
   val path = "./valid/path"
   val otherPath = "./some/path"
   
+  when(mockPathValidator.validate(
+    Matchers.matches(path)))
+    .thenReturn(true)
+
+  when(mockPathValidator.validate(
+    not(Matchers.eq(path))))
+    .thenReturn(false)
+    
   describe("the validatePath method") {
     it("should return Some") {
       Given("a path that exists")
@@ -47,43 +47,68 @@ class ValMedTester extends FunSpec
     it("should return the path String") {
       Given("a path which exists")
       Then("it should return return the path")
-      assert(vm.validatePathOrThrow(path,mockPathValidator) == path)
+      assert(vm.validatePathOrThrow(("valid",path),mockPathValidator) == path)
     }
     
     it("should throw an exception") {
       Given("a path which does not exist")
       Then("it should throw an exception")
-      assertThrows[Exception](vm.validatePathOrThrow(otherPath,mockPathValidator))
+      assertThrows[Exception](vm.validatePathOrThrow(("not valid",otherPath),mockPathValidator))
       And("it should message the user")
       Mockito.verify(mockGui,Mockito.times(1)).message(Matchers.anyString())
     }
   }
   
   describe("the validateAllPaths method") {
+    val pathMessage = "Could not reach the %s. Please check if path is correct" +
+      ", or report this issue"
+
     it("should validate all paths entered by the user") {
       Given("existing paths for the details and template file, and the destination folder")
-      Then("it should message the user that all paths are valid")
-      assert(false)
+      when(mockGui.detailsFile).thenReturn(path)
+      when(mockGui.templateFile).thenReturn(path)
+      when(mockGui.destinationFolder).thenReturn(path)
+
+      Then("it should message the user")
+      vm.validateAllPaths(mockPathValidator)
+      Mockito.verify(mockGui,Mockito.times(1)).message("File paths validated")
     }
     
-    it("should throw an exception") {
+    it("should throw an exception if it can't reach the details file") {
       Given("an invalid details file path")
-      Then("it should message the user that details cannot be found")
-      assert(false)
-      And("it should throw an exception")
-      assert(false)
+      when(mockGui.detailsFile).thenReturn(otherPath)
+      when(mockGui.templateFile).thenReturn(path)
+      when(mockGui.destinationFolder).thenReturn(path)
 
+      Then("it should throw an exception")
+      assertThrows[Exception](vm.validateAllPaths(mockPathValidator))
+      And("it should message the user that the details file cannot be found")
+      Mockito.verify(mockGui,Mockito.times(1)).message(pathMessage.format("details file"))
+    }
+
+    it("should throw an exception if it can't reach the template file") {
       Given("an invalid template file path")
-      Then("it should message the user that the template cannot be found")
-      assert(false)
-      And("it should throw an exception")
-      assert(false)
+      when(mockGui.detailsFile).thenReturn(path)
+      when(mockGui.templateFile).thenReturn(otherPath)
+      when(mockGui.destinationFolder).thenReturn(path)
 
+      Then("it should throw an exception")
+      assertThrows[Exception](vm.validateAllPaths(mockPathValidator))
+      And("it should message the user that the template file cannot be found")
+      Mockito.verify(mockGui,Mockito.times(1)).message(pathMessage.format("template file"))
+    }
+
+    it("should throw an exception if it can't reach the destination folder") {
       Given("an invalid destination folder path")
-      Then("it should message the user that the destination cannot be found")
-      assert(false)
-      And("it should throw an exception")
-      assert(false)
+      when(mockGui.detailsFile).thenReturn(path)
+      when(mockGui.templateFile).thenReturn(path)
+      when(mockGui.destinationFolder).thenReturn(otherPath)
+
+
+      Then("it should throw an exception")
+      assertThrows[Exception](vm.validateAllPaths(mockPathValidator))
+      And("it should message the user that the destination folder cannot be found")
+      Mockito.verify(mockGui,Mockito.times(1)).message(pathMessage.format("destination folder"))
     }
   }
 }
