2ff18c101545629ecccfe68cf1dccd5c6fbb5f3c
claudiusbr <claudiusbr@users.noreply.github.com>
2017-08-30 21:57:14 +0100

    green test for validatePath method

--- src/test/scala/lettergenerator/mediator/ValMedTester.scala
+++ src/test/scala/lettergenerator/mediator/ValMedTester.scala
@@ -6,7 +6,8 @@ import validators._
 
 import org.scalatest.{FunSpec, GivenWhenThen}
 import org.mockito.Mockito.when
-import org.mockito.Mockito
+import org.mockito.{Mockito, Matchers}
+import org.mockito.AdditionalMatchers.not
 import org.scalatest.mockito.MockitoSugar
 
 class ValMedTester extends FunSpec 
@@ -15,17 +16,31 @@ class ValMedTester extends FunSpec
   val mockGui: Wizard = mock[Wizard]
   val mockPathValidator: PathValidator = mock[PathValidator]
   
+  val vm = new ValidationMediator(mockGui)
+  
+  when(mockPathValidator.validate(
+    Matchers.matches("./valid/path")))
+    .thenReturn(true)
+
+  when(mockPathValidator.validate(
+    not(Matchers.eq("./valid/path"))))
+    .thenReturn(false)
+
+  val path = "./valid/path"
+  val otherPath = "./some/path"
+  
   describe("the validatePath method") {
     it("should return Some") {
       Given("a path that exists")
+
       Then("it returns Some(path)")
-      assert(false)
+      assert(vm.validatePath(path, mockPathValidator).get == "./valid/path")
     }
     
     it("should return None") {
       Given("a path which does not exist")
       Then("it returns None")
-      assert(false)
+      assert(vm.validatePath(otherPath, mockPathValidator) == None)
     }
   }
   
