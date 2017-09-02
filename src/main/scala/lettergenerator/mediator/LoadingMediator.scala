df46135b9c0637e2226782b1e2eadf13109b3084
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-02 08:51:08 +0100

    green: implementation of loadTemplate method

--- src/main/scala/lettergenerator/mediator/LoadingMediator.scala
+++ src/main/scala/lettergenerator/mediator/LoadingMediator.scala
@@ -4,8 +4,13 @@ package mediator
 import loader._
 import formatter._
 
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage
+
 class LoadingMediator(gui: renderer.Wizard) {
 
   def loadDetails(form: DetailsFormatter = new DetailsFormatter(
     new CsvInput(gui.detailsFile))): List[Map[String,String]] = form.details
+    
+  def loadTemplate(form: TemplateFormatter = new TemplateFormatter(
+    new DocxInput(gui.templateFile))): WordprocessingMLPackage = form.template
 }
