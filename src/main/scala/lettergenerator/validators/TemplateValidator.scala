2b1566634401eb3b6d06595d77276ed72e3a7548
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-04 00:14:59 +0100

    removed debugging code

--- src/main/scala/lettergenerator/validators/TemplateValidator.scala
+++ src/main/scala/lettergenerator/validators/TemplateValidator.scala
@@ -2,7 +2,6 @@ package lettergenerator
 package validators
 
 case class TemplateValidator(text: String) extends RecursiveValidator {
-  println(text)
   def validate(what: Any): Boolean = {
     val detailsHeader = what.asInstanceOf[String]
     validateVariables(detailsHeader)
