79fcc924ed40d9730a5d1b00e0a83c927bafcdca
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-05 00:22:44 +0100

    shell class for formatter tests

---
+++ src/test/scala/lettergenerator/formatter/DocxMkrFmtrTester.scala
@@ -0,0 +1,13 @@
+package lettergenerator
+package formatter
+
+import org.scalatest.{FunSpec, GivenWhenThen}
+import org.mockito.Mockito.when
+import org.mockito.{Mockito, Matchers}
+import org.mockito.AdditionalMatchers.not
+import org.scalatest.mockito.MockitoSugar
+
+class DocxMkrFmtrTester extends FunSpec
+  with MockitoSugar with GivenWhenThen {
+  
+}
