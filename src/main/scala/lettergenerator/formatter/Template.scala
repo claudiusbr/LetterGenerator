bf9ea72fcd93aeef3e070cd9bed236433e496370
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-09 16:47:04 +0100

    making own type for Template

---
+++ src/main/scala/lettergenerator/formatter/Template.scala
@@ -0,0 +1,17 @@
+package lettergenerator
+package formatter
+
+import org.docx4j.XmlUtils
+import org.docx4j.wml.Document
+import org.docx4j.openpackaging.io.SaveToZipFile
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart
+
+class Template(val docPack: WordprocessingMLPackage) {
+  def mainDocumentPart: MainDocumentPart = docPack.getMainDocumentPart
+
+  def jaxbElement: Document  = mainDocumentPart.getJaxbElement
+
+  def jaxbElement_= (replacement: Document): Unit = 
+    mainDocumentPart.setJaxbElement(replacement)
+}
