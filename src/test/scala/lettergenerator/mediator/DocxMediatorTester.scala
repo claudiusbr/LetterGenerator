1a66ae127fcb88c826a703ae87733a2a9276a498
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-10 20:06:57 +0100

    red: test for DocxMediator

--- src/test/scala/lettergenerator/mediator/DocxMediatorTester.scala
+++ src/test/scala/lettergenerator/mediator/DocxMediatorTester.scala
@@ -2,7 +2,10 @@ package lettergenerator
 package mediator
 
 import renderer.Wizard
-import formatter.{Template,DocxMakerFormatter}
+import formatter.{Template,DocxMakerFormatter, Details}
+import generators._
+
+import scala.collection.JavaConverters._
 
 import org.docx4j.XmlUtils
 import org.docx4j.wml.Document
@@ -17,6 +20,8 @@ import org.mockito.Matchers.{anyString,anyInt}
 import org.mockito.AdditionalMatchers.not
 import org.scalatest.mockito.MockitoSugar
 
+import java.util.{HashMap => JHashMap}
+
 class DocxMediatorTester extends FunSpec
   with GivenWhenThen with MockitoSugar {
 
@@ -25,12 +30,47 @@ class DocxMediatorTester extends FunSpec
   val mockGui = testObjects.mockGui
   val mockTempl = testObjects.mockTempl
   val mockDMForm = mock[DocxMakerFormatter]
-  val dm = new DocxMediator(mockGui,mockTempl,mockDMForm)
+  val mockGen = mock[Generator]
+  val mockVM = mock[ValidationMediator]
+  val mockSaver = mock[SaveToZipFile]
+  val dm = new DocxMediator(mockGui,mockTempl,mockDMForm,mockGen)
+
+  val detailsSingleTuple: Map[String,String] = testObjects.details.tuples.head
+  val singleTupleAsJMap = new JHashMap[String,String](detailsSingleTuple.asJava)
+
+  when(mockGui.fnAlsoInTemplate).thenReturn(true)
+  when(mockGui.fNameColumn).thenReturn(testObjects.details.tuples.head.keys.head)
+  when(mockDMForm.prepareMap(detailsSingleTuple, mockGui.fnAlsoInTemplate, mockGui.fNameColumn))
+      .thenReturn(singleTupleAsJMap)
+
+  when(mockDMForm.fileName(detailsSingleTuple,mockGui.fNameColumn))
+    .thenReturn("filename")
+
+  when(mockVM.fileNameIfDuplicate("filename", ".docx")).thenReturn("filename")
+
+  //when(mockGen.generate(singleTupleAsJMap,"filename")(mockSaver)).thenReturn()
   
   describe("the generateDocx method") {
     it("generates a single docx") {
       Given("a single details tuple")
-      
+      When("the method is called")
+      dm.generateDocx(detailsSingleTuple,mockVM)(mockSaver)
+
+      Then("a single call to DocxGenerator.generate should be made")
+      Mockito.verify(mockGen,Mockito.times(1))
+        .generate(singleTupleAsJMap,"filename")(mockSaver)
+    }
+
+    it("generates two docx") {
+      Given("two details tuples")
+      val twoTuples = testObjects.details
+
+      When("the method is called")
+      dm.generateDocx(twoTuples,mockVM)(mockSaver)
+
+      Then("two calls should be made to DocxGenerator.generate")
+      Mockito.verify(mockGen,Mockito.times(2))
+        .generate(singleTupleAsJMap,"filename")(mockSaver)
     }
   }
 }
