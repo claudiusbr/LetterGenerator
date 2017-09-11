fe5a64f419edfcf4646fb3a7944d0b9d1c197fa6
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-11 21:48:57 +0100

    added TODO and test shell

--- src/test/scala/lettergenerator/formatter/DocxMkrFmtrTester.scala
+++ src/test/scala/lettergenerator/formatter/DocxMkrFmtrTester.scala
@@ -10,4 +10,20 @@ import org.scalatest.mockito.MockitoSugar
 class DocxMkrFmtrTester extends FunSpec
   with MockitoSugar with GivenWhenThen {
   
+  val dmForm = new DocxMakerFormatter
+  
+  describe("the prepareMap method") {
+    it("returns a JHashMap with the file name column included") {
+      Given("the value true for fileNameAlsoInTemplate")
+      val fnInTemplate: Boolean = true
+      
+      When("")
+    }
+    it("returns a JHashMap without the file name column") {}
+  }
+
+  describe("the fileName method") {
+    it("returns the file name chosen") {}
+    it("returns Output") {}
+  }
 }
