5396e339d79cdaff2c26e2af0f1ed26a23a2aacc
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-13 00:09:11 +0100

    red test for Details auxiliary constructor

---
+++ src/test/scala/lettergenerator/formatter/DetailsTester.scala
@@ -0,0 +1,26 @@
+package lettergenerator
+package formatter
+
+import org.scalatest.{FunSpec, GivenWhenThen}
+
+class DetailsTester extends FunSpec with GivenWhenThen {
+  
+  val testObjects = new TestObjects
+  
+  describe("the auxiliary constructor") {
+    it("should pass the keys of the head of the tuples list as headers if none is provided") {
+      Given("a list of tuples")
+      val tuples = testObjects.tuples
+      
+      When("they are passed to the constructor of the Details class")
+      val headers: Array[String] = tuples.head.keys.toArray
+      val details = new Details(tuples)
+      
+      Then("a new Details class should be created")
+      assert(details.isInstanceOf[Details])
+      
+      And("it should have a field called \"headers\" equal to the keys of the head of the tuples list")
+      assert(details.headers.deep == headers.deep)
+    }
+  }
+}
--- src/test/scala/lettergenerator/formatter/DocxMkrFmtrTester.scala
+++ src/test/scala/lettergenerator/formatter/DocxMkrFmtrTester.scala
@@ -4,7 +4,6 @@ package formatter
 import org.scalatest.{FunSpec, GivenWhenThen}
 import org.mockito.Mockito.when
 import org.mockito.{Mockito, Matchers}
-import org.mockito.AdditionalMatchers.not
 import org.scalatest.mockito.MockitoSugar
 
 import java.util.{HashMap => JHashMap}
