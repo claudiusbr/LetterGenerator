d7a15ff36893047506a0f4013b6246124dbfdf0e
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-13 00:16:57 +0100

    green test Details auxiliary constructor

--- src/main/scala/lettergenerator/formatter/Details.scala
+++ src/main/scala/lettergenerator/formatter/Details.scala
@@ -1,4 +1,8 @@
 package lettergenerator
 package formatter
 
-class Details(val headers: Array[String], val tuples: List[Map[String,String]])
+class Details(val headers: Array[String], val tuples: List[Map[String,String]]) {
+  def this(tuples: List[Map[String,String]]) {
+    this(tuples.head.keys.toArray,tuples)
+  }
+}
