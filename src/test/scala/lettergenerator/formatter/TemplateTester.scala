8baf0f9326d175b6173451dbec16f77771f21288
claudiusbr <claudiusbr@users.noreply.github.com>
2017-09-13 20:33:46 +0100

    green test for jaxbElement

--- src/test/scala/lettergenerator/formatter/TemplateTester.scala
+++ src/test/scala/lettergenerator/formatter/TemplateTester.scala
@@ -16,12 +16,14 @@ class TemplateTester extends FunSpec
   with GivenWhenThen with MockitoSugar {
   
   val mockJaxbElement = mock[Document]
+  val mockNewJaxbElement = mock[Document]
   val mockMainPart = mock[MainDocumentPart]
   val mockDocPack = mock[WordprocessingMLPackage]
 
-  val template = new Template(mockDocPack)
-  
   when(mockDocPack.getMainDocumentPart).thenReturn(mockMainPart)
+  when(mockMainPart.getJaxbElement).thenReturn(mockJaxbElement)
+  
+  val template = new Template(mockDocPack)
   
   describe("the docPack field") {
     it("returns the WordprocessingMLPackage passed as an argument to the constructor") {
@@ -38,10 +40,34 @@ class TemplateTester extends FunSpec
     it("returns the template's MainDocumentPart") {
       Given("an instance of the Template class")
       When("the method is called")
-      val mainPart = template.mainDocumentPart
+      val testMainPart = template.mainDocumentPart
       
       Then("it returns a MainDocumentPart instance corresponding to the template")
-      assert(mainPart == mockMainPart)
+      assert(testMainPart == mockMainPart)
+    }
+  }
+  
+  describe("the jaxbElement method") {
+    it("returns the content of the template") {
+      Given("an instance of the Template class")
+      
+      When("the method is called with no arguments")
+      val testDoc = template.jaxbElement
+      
+      Then("it returns the Document instance with the template's contents")
+      assert(testDoc == mockJaxbElement)
+    }
+    
+    it("registers new contents to the WordprocessingMLPackage instance") {
+      Given("a new instance of Document")
+      val newDoc = mockNewJaxbElement
+      
+      When("the method is called with a new instance of Document")
+      template.jaxbElement = newDoc
+      
+      Then("the new instance should be registered in the WordprocessingMLPackage")
+      Mockito.verify(mockMainPart,Mockito.times(1)).setJaxbElement(newDoc)
+      
     }
   }
   
